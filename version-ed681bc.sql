ba7015be3844e74a977fa0509f3da51969b0fdde
joaquinchecura <joaquinchecura@gmail.com>
2026-09-08 13:16:25 -0300

    3

---
+++ version-ed681bc.sql
@@ -0,0 +1,3 @@
+﻿-- AlterTable: memberId now nullable + add isTemplate
+ALTER TABLE "routines" ALTER COLUMN "memberId" DROP NOT NULL;
+ALTER TABLE "routines" ADD COLUMN IF NOT EXISTS "isTemplate" BOOLEAN NOT NULL DEFAULT false;
